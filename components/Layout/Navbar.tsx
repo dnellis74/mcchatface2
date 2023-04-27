aefc87bc06adccd66d273aad0b38442b140150cb
David Nellis <dnellis@woodridgesoftware.com>
2023-04-27 10:48:01 -0600

    Manually brought over Simon's work

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -2,8 +2,8 @@ import { FC } from "react";
 
 export const Navbar: FC = () => {
   return (
-    <div className="flex h-[50px] sm:h-[60px] border-b border-neutral-300 py-2 px-2 sm:px-8 items-center justify-between">
-      <div className="font-bold text-3xl flex items-center">
+    <div className="flex h-[50px] sm:h-[60px] border-neutral-300 py-2 px-2 sm:px-8 items-center justify-between navbar-top">
+      <div className="font-bold text-3xl flex items-center site-branding">
         <a
           className="ml-2 hover:opacity-50"
           href="https://code-scaffold.vercel.app"
