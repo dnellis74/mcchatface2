cc23834a8de86aca53f1d34f1be64f3a5e3f6183
simonindelicate <78307744+simonindelicate@users.noreply.github.com>
2023-04-27 17:10:30 +0100

    user inpuit color and footer links

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,5 +1,9 @@
 import { FC } from "react";
 
 export const Footer: FC = () => {
-  return <div className="flex h-[30px] sm:h-[50px] border-neutral-300 py-2 px-8 items-center sm:justify-between justify-center footer-bottom"></div>;
+  return <div className="flex h-[30px] sm:h-[50px] border-neutral-300 py-2 px-8 items-center sm:justify-between justify-center footer-bottom">
+
+<p>&copy; 2023 ChatTHC.ai | <a href="privacy.html">Privacy Policy</a> | <a href="about.html">About ChatTHC</a></p>
+
+  </div>;
 };
