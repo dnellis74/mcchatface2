455cf0adbd0a1953fec80b1278ad18fbed0a6687
simonindelicate <78307744+simonindelicate@users.noreply.github.com>
2023-04-28 12:50:27 +0100

    added lava lamp, fixed background size issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -142,7 +142,32 @@ export default function Home() {
 
 
             <div className="flex-1 lavalamp">
-Lava Lamp goes here
+
+
+<div class="lamp">
+  <div class="glass">
+    <div class="lava">
+      <div class="blob"></div>
+      <div class="blob"></div>
+      <div class="blob"></div>
+      <div class="blob top"></div>
+      <div class="blob bottom"></div>
+    </div>
+  </div>
+</div>
+
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+  <defs>
+    <filter id="goo">
+      <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+      <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+      <feBlend in="SourceGraphic" in2="goo" />
+    </filter>
+  </defs>
+</svg>
+
+
+
 </div>
 
         </div>
